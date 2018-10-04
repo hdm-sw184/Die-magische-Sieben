f6ed775340e50e6f081ba9761dfd56cb6f256021
hdm-ss440 <37869789+hdm-ss440@users.noreply.github.com>
2018-10-04 22:14:50 +0200

    Add files via upload

---
+++ Unterstützernetzwerk_Code.R
@@ -0,0 +1,18 @@
+# Beispiel Gesamtnetzwerk Studierende (Work- und Help-Netzwerk)
+library(igraph)
+# liest die Dateien direkt aus dem github-Verzeichnis ein
+el <- read.csv("https://raw.githubusercontent.com/hdm-sw184/Die-magische-Sieben/master/226305_Projekt_WashNetzwerk_Personennetzwerk_Edgelist%20-%20Tabellenblatt1.csv", header=T, as.is=T, sep = ",")
+nodes <- read.csv("https://raw.githubusercontent.com/hdm-sw184/Die-magische-Sieben/master/226305_Projekt_WashNetzwerk_Personennetzwerk_Nodelist%20-%20Tabellenblatt1.csv", header=T, as.is=T, sep = ",")
+# prüft, ob alle Variablen eingelesen wurden
+head(el)
+head(nodes)
+# wandelt die edgelist in eine Matrix um und baut das igraph-Objekt
+hties <- as.matrix(el)
+students <- graph_from_data_frame(d=hties, vertices=NULL, directed=F)
+students
+# addiert edges auf, wenn sie auf der gleichen Beziehung sind
+s <- simplify(students, edge.attr.comb = list(weight="sum"))
+# ruft das finale igraph-Objekt auf.
+s
+# einfache Visualisierung
+plot(s)
